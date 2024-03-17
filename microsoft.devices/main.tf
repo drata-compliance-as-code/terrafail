ab0eec0bb7526c9f41c6f2a9c10861294ae99a50
mo-drata <morgansuch@drata.com>
2024-03-16 23:19:24 -0500

    feature(TF): insecure TF test 8006

---
+++ microsoft.devices/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "iothub_rg" {
+  name     = "iothub_rg"
+  location = "East US"
+}
+
+# ---------------------------------------------------------------------
+# Device
+# ---------------------------------------------------------------------
+resource "azurerm_iothub" "sac_iothub" {
+  name                = "sac-testing-iothub"
+  resource_group_name = azurerm_resource_group.iothub_rg.name
+  location            = azurerm_resource_group.iothub_rg.location
+  sku {
+    name     = "S1"
+    capacity = "1"
+  }
+  public_network_access_enabled = true # SaC Testing - Severity: Critical - Set public_network_access_enabled to true
+  min_tls_version               = 1.2  # SaC Testing - Severity: Critical - Set min_tls_version != 1.2
+  network_rule_set {
+    default_action = "Allow"
+    ip_rule { # SaC Testing - Severity: High - Set ip_rule to undefined
+      name    = "wildcard"
+      ip_mask = "0.0.0.0/0" # SaC Testing - Severity: Moderate - Set ip_mask to blacklisted IP
+    }
+  }
+  # SaC Testing - Severity: Moderate - Set tags to undefined
+  # tags = {
+  #   purpose = "testing"
+  # }
+}
