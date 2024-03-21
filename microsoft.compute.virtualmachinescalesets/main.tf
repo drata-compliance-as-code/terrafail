e17a5318c671e705b6efaf2c89a3757df794e97c
mo-drata <morgansuch@drata.com>
2024-03-21 14:12:15 -0500

    removing duplicate windows/linux configurations

--- microsoft.compute.virtualmachinescalesets/main.tf
+++ microsoft.compute.virtualmachinescalesets/main.tf
@@ -6,61 +6,6 @@ resource "azurerm_resource_group" "vmss_resource_group" {
 # ---------------------------------------------------------------------
 # Virtual Machine Scale Set
 # ---------------------------------------------------------------------
-resource "azurerm_linux_virtual_machine_scale_set" "sac_linux_vmss" {
-  name                            = "sac-linux"
-  resource_group_name             = azurerm_resource_group.vmss_resource_group.name
-  location                        = azurerm_resource_group.vmss_resource_group.location
-  sku                             = "Standard_A1_v2"
-  instances                       = 1
-  admin_username                  = "adminuser"
-  disable_password_authentication = false
-  admin_password                  = "P@55w0rd1234!" # SaC Testing - Severity: High - Set admin_password != ""
-  encryption_at_host_enabled      = false           # SaC Testing - Severity : Critical - Set encryption_at_host_enabled to false
-  #health_probe_id = azurerm_lb_probe.vmss_lb_probe.id  # SaC Testing - Severity: Moderate - Set health_probe_id to undefined
-  upgrade_mode = "Automatic"
-  #zones = ["2"]  # SaC Testing - Severity: Moderate - Set zones == ""
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-  automatic_instance_repair {
-    enabled = false # SaC Testing - Severity: High - Set enabled to false
-  }
-  automatic_os_upgrade_policy {
-    enable_automatic_os_upgrade = false # SaC Testing - Severity: High - Set enable_automatic_os_upgrade to false
-    disable_automatic_rollback  = true
-  }
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-    #disk_encryption_set_id = azurerm_disk_encryption_set.sac_vmss_disk_encryption_set.id # SaC Testing - Severity: Moderate - Set disk_encryption_set_id to ""
-  }
-  data_disk {
-    caching              = "None"
-    disk_size_gb         = 3
-    lun                  = 20
-    storage_account_type = "Standard_LRS"
-    #disk_encryption_set_id = azurerm_disk_encryption_set.sac_vmss_disk_encryption_set.id # SaC Testing - Severity: Moderate - Set disk_encryption_set_id to ""
-  }
-  network_interface {
-    name    = "example"
-    primary = true
-    #network_security_group_id = azurerm_network_security_group.vmss_network_security_group.id  # SaC Testing - Severity: High - Set network_security_group_id to ""
-    ip_configuration {
-      name                                   = "internal"
-      primary                                = true
-      subnet_id                              = azurerm_subnet.vmss_subnet.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
-    }
-  }
-  # SaC Testing - Severity: Moderate - set tags to undefined
-  # tags = {
-  #   environment = "staging"
-  # }
-}
-
 resource "azurerm_windows_virtual_machine_scale_set" "sac_windows_vmss" {
   name                       = "sac-windo"
   resource_group_name        = azurerm_resource_group.vmss_resource_group.name
