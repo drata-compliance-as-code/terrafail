a8617c512efebbb5a5b483b5128c0e85b800fdd3
Drata-Action-Bot-Local <actionbot-drata@drata.com>
2024-04-05 15:28:31 -0500

    Drata Compliance Recommendations (Grouped)

--- aws::kinesis/main.tf
+++ aws::kinesis/main.tf
@@ -13,7 +13,7 @@ resource "aws_kinesis_stream" "sac_kinesis_stream" {
   stream_mode_details {
     stream_mode = "PROVISIONED"
   }
-  encryption_type = "NONE" # SaC Testing - Severity: Moderate - set tags to undefined
+  encryption_type = "KMS"
   #kms_key_id = aws_kms_key.kinesis_key.id  # SaC Testing - Severity: Moderate - set kms_key_id to undefined
   # SaC Testing - Severity: Moderate - set tags to undefined
   # tags = {
