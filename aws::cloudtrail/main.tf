8f382974eff90967cfadbdd1d008b26c3adcf864
Drata-Action-Bot-Local <actionbot-drata@drata.com>
2024-04-05 15:51:36 -0500

    Drata Compliance Recommendations (Grouped)

--- aws::cloudtrail/main.tf
+++ aws::cloudtrail/main.tf
@@ -5,7 +5,7 @@
 resource "aws_cloudtrail" "sac_cloudtrail" {
   name                          = "sac_cloudtrail"
   s3_bucket_name                = aws_s3_bucket.sac_testing.id
-  enable_log_file_validation = false    # SaC Testing - Severity: High - Set enable_log_file_validation to false
+  enable_log_file_validation = true
   is_multi_region_trail = false # SaC Testing - Severity: Moderate - Set is_multi_region_trail to false
 
 #   tags = {  # SaC Testing - Severity: Moderate - Set tags to undefined
