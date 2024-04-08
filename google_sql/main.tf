be50dbf9ed34e3252aa8f20c75805a034d298eaa
Drata-Action-Bot-Local <actionbot-drata@drata.com>
2024-04-08 11:36:17 -0500

    Drata Compliance Recommendations (Grouped)

--- google_sql/main.tf
+++ google_sql/main.tf
@@ -17,7 +17,7 @@ resource "google_sql_database_instance" "sac_sql_db_instance" {
         value = "*" # SaC Testing - Severity: High - set value to '*'
       }
       ipv4_enabled = true  # SaC Testing - Severity: Moderate - set ipv4_enabled to True
-      require_ssl  = false # SaC Testing - Severity: High - set require_ssl to False
+      require_ssl  = true
     }
     backup_configuration {
       enabled                        = false # SaC Testing - Severity: Moderate - set enabled to False
