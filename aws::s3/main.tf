a2ad9391904191befadff64636464906bee05212
Drata-Action-Bot-Local <actionbot-drata@drata.com>
2024-04-08 11:39:29 -0500

    Drata Compliance Recommendations (Grouped)

--- aws::s3/main.tf
+++ aws::s3/main.tf
@@ -65,7 +65,7 @@ resource "aws_s3_bucket_public_access_block" "s3_public_access_block_sac" { # Sa
   bucket                  = aws_s3_bucket.s3_bucket_sac.id
   block_public_acls       = false # SaC Testing - Severity: Critical - Set block_public_acls to false
   block_public_policy     = false # SaC Testing - Severity: High - Set block_public_policy to false
-  ignore_public_acls      = false # SaC Testing - Severity: Critical - Set ignore_public_acls to false
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
