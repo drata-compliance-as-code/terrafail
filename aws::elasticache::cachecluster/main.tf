d3c255b29c0e0a5e9cfef0e88cb7bd5942931c07
Drata-Action-Bot-Local <actionbot-drata@drata.com>
2024-04-08 11:28:23 -0500

    Drata Compliance Recommendations (Grouped)

--- aws::elasticache::cachecluster/main.tf
+++ aws::elasticache::cachecluster/main.tf
@@ -12,7 +12,7 @@ resource "aws_elasticache_cluster" "sac_memcached_cluster" {
   port                     = 11211 # SaC Testing - Severity: Low - set port to default (11211)
   subnet_group_name        = aws_elasticache_subnet_group.elasticache_subnet_group.name
   snapshot_retention_limit = 0           # SaC Testing - Severity: Moderate - set snapshot_retention_limit = 0
-  az_mode                  = "single-az" # SaC Testing - Severity: High - set az_mode != 'cross-az'
+  az_mode                  = "cross-az"
   #security_group_ids = [aws_security_group.cluster_security_group.id]  # SaC Testing - Severity: Moderate - set security_group_ids to undefined
   # SaC Testing - Severity: Moderate - set tags to undefined
   # tags = {
