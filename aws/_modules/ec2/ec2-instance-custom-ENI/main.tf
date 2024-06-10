2d28a452a739e5090691becdefb391d988570b7d
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/ec2/ec2-instance-custom-ENI/main.tf
+++ aws/_modules/ec2/ec2-instance-custom-ENI/main.tf
@@ -106,5 +106,6 @@ resource "aws_security_group" "TerraFailEC2Instance_security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  # Drata: Ensure that [aws_security_group.egress.cidr_blocks] is explicitly defined and narrowly scoped to only allow traffic to trusted sources
   }
 }
