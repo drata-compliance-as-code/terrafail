03b01bf6af3665b80bfafa74b1635d50c453ccf8
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/elbv1/main.tf
+++ aws/_modules/elbv1/main.tf
@@ -10,7 +10,7 @@ resource "aws_elb" "TerraFailELB" {
 
   listener {
     instance_port     = 8000
-    instance_protocol = "HTTP"
+    instance_protocol = "HTTPS"
     lb_port           = 80
     lb_protocol       = "HTTPS"
   }
