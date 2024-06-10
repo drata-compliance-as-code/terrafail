f20312e83e9ba9b3b3d954f43669c5eb96257f25
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/sql_database/main.tf
+++ azure/_modules/sql_database/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_mssql_database" "TerraFailSQL_database" {
 
 resource "azurerm_mssql_database_extended_auditing_policy" "TerraFailSQL_database_auditing_policy" {
   database_id       = azurerm_mssql_database.TerraFailSQL_database.id
-  enabled           = false
+  enabled           = true
   retention_in_days = 365
 }
 
