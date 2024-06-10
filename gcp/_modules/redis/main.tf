4891a3c1287e0124b2306f052c26d701bde7e5fd
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:06 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/redis/main.tf
+++ gcp/_modules/redis/main.tf
@@ -2,6 +2,7 @@
 # Redis
 # ---------------------------------------------------------------------
 resource "google_redis_instance" "TerraFailRedis" {
+  # Drata: Set [configId] to ensure that organization-wide label conventions are followed.
   name                    = "TerraFailRedis"
   memory_size_gb          = 1
   tier                    = "BASIC"
