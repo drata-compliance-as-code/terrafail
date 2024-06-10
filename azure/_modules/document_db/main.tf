063c93c960a0f4e6a596cd6c1b0cb4dfaa5efa41
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/document_db/main.tf
+++ azure/_modules/document_db/main.tf
@@ -100,6 +100,7 @@ resource "azurerm_storage_account" "TerraFailCosmosDB_storage" {
   resource_group_name      = azurerm_resource_group.TerraFailCosmosDB_rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  # Drata: Configure [azurerm_storage_account.account_replication_type] to improve infrastructure availability and resilience. To create highly available Storage Accounts, set azurerm_storage_account.account_replication_type to a geo-redundant storage option by selecting one of the following SKUs: ['standard_grs', 'standard_gzrs', 'standard_ragrs', 'standard_ragzrs', 'grs', 'gzrs', 'ragrs', 'ragzrs']
 
   tags = {
     environment = "staging"
