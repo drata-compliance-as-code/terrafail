925cf169675ab704fb30b23cf5946089232ba612
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:06 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/elbv1/main.tf
+++ aws/_modules/elbv1/main.tf
@@ -4,6 +4,7 @@
 # ELBv1
 # ---------------------------------------------------------------------
 resource "aws_elb" "TerraFailELB" {
+  # Drata: Default network security groups allow broader access than required. Specify [aws_elb.security_groups] to configure more granular access control
   name     = "TerraFailELB"
   subnets  = [aws_subnet.TerraFailELB_subnet.id]
   # Drata: Configure [aws_elb.subnets] to improve infrastructure availability and resilience. Define at least 2 subnets or availability zones on your load balancer to enable zone redundancy
