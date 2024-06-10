7e20f4d8ee6f63877903fbd26f2152f133ef60d0
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/aks/main.tf
+++ azure/_modules/aks/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "TerraFailAKS_node_pool" {
   name                  = "TerraFailAKS_node_pool"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.TerraFailAKS_cluster.id
   vm_size               = "Standard_DS2_v2"
-  enable_node_public_ip = true
+  enable_node_public_ip = false
   enable_auto_scaling   = true
   max_count             = 100
   min_count             = 0
