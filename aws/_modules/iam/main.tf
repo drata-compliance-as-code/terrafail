bfe6a58204cf6c4b9c7fa08f3e3c4ba6fe9019ae
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/iam/main.tf
+++ aws/_modules/iam/main.tf
@@ -95,6 +95,7 @@ resource "aws_iam_role_policy" "TerraFailIAM_role_policy" {
 resource "aws_iam_role" "TerraFailIAM_role_inline" {
   name = "TerraFailIAM_role_inline"
   assume_role_policy = jsonencode({
+    # Drata: Explicitly define principals for [aws_iam_role.assume_role_policy] in adherence with the principal of least privilege. Avoid the use of overly permissive allow-all access patterns such as (*)
     Version = "2012-10-17"
     Statement = [
       {
