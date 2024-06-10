a87e2dab342e6f5155d724a51323b9c8239b541c
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/cache/main.tf
+++ azure/_modules/cache/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_redis_cache" "TerraFailCache" {
   capacity                      = 1
   family                        = "P"
   sku_name                      = "Premium"
-  enable_non_ssl_port           = true
+  enable_non_ssl_port           = false
   minimum_tls_version           = "1.0"
   public_network_access_enabled = true
 
