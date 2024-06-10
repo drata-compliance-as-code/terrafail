a932812fa321b1786f568f181001aca121ec6a34
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:03 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/elbv2/elbv2-application/main.tf
+++ aws/_modules/elbv2/elbv2-application/main.tf
@@ -248,6 +248,7 @@ resource "aws_launch_template" "TerraFailLB_launch_template" {
 # KMS
 # ---------------------------------------------------------------------
 resource "aws_kms_key" "TerraFailLB_key" {
+  # Drata: Define [aws_kms_key.policy] to restrict access to your resource. Follow the principal of minimum necessary access, ensuring permissions are scoped to trusted entities. Exclude this finding if access to Keys is managed using IAM policies instead of a Key policy
   # Drata: Set [aws_kms_key.tags] to ensure that organization-wide tagging conventions are followed.
   description             = "TerraFailLB_key"
   deletion_window_in_days = 10
