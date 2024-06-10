30e346b9a21f3cb7f805ba34fc8b3caca803d8f6
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/sql/main.tf
+++ gcp/_modules/sql/main.tf
@@ -15,6 +15,7 @@ resource "google_sql_database_instance" "TerraFailSQL_instance" {
     ip_configuration {
       authorized_networks {
         value = "*"
+      # Drata: Ensure that [google_sql_database_instance.settings.ip_configuration.authorized_networks.value] is explicitly defined and narrowly scoped to only allow trusted sources to access SQL Database
       }
       ipv4_enabled = true
       require_ssl  = true
