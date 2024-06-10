6e383f6dae383eff9f0bcccad5f5b6b412c05537
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/redis/main.tf
+++ gcp/_modules/redis/main.tf
@@ -6,5 +6,5 @@ resource "google_redis_instance" "TerraFailRedis" {
   memory_size_gb          = 1
   tier                    = "BASIC"
   auth_enabled            = false
-  transit_encryption_mode = "DISABLED"
+  transit_encryption_mode = "SERVER_AUTHENTICATION"
 }
