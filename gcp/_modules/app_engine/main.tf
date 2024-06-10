762fcf9a9a8882378f548985a64d2a44e4775ebe
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/app_engine/main.tf
+++ gcp/_modules/app_engine/main.tf
@@ -24,7 +24,7 @@ resource "google_app_engine_flexible_app_version" "TerraFailAppEngine_version_fl
     path = "/"
   }
   api_config {
-    security_level = "SECURE_DEFAULT"
+    security_level = "SECURE_ALWAYS"
     script         = "path/to/script.py"
   }
   handlers {
