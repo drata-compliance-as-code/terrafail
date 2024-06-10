bd1d5afc9747f03472736b5d6f105acc73630ce1
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/cache/main.tf
+++ azure/_modules/cache/main.tf
@@ -50,6 +50,7 @@ resource "azurerm_storage_account" "TerraFailCache_storage" {
   location                      = azurerm_resource_group.TerraFailCache_rg.location
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
+  # Drata: Configure [azurerm_storage_account.account_replication_type] to improve infrastructure availability and resilience. To create highly available Storage Accounts, set azurerm_storage_account.account_replication_type to a geo-redundant storage option by selecting one of the following SKUs: ['standard_grs', 'standard_gzrs', 'standard_ragrs', 'standard_ragzrs', 'grs', 'gzrs', 'ragrs', 'ragzrs']
   public_network_access_enabled = false
 }
 
