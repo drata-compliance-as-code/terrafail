adb659d8563541d369f1bfd3a23fc19d45049ede
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/elbv1/main.tf
+++ aws/_modules/elbv1/main.tf
@@ -52,6 +52,7 @@ resource "aws_security_group" "TerraFailELB_security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  # Drata: Ensure that [aws_security_group.egress.cidr_blocks] is explicitly defined and narrowly scoped to only allow traffic to trusted sources
   }
 }
 
