bd147d124bc3597bf1da7cfa7f48befd20b2bcf7
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/rds/rds-instance/main.tf
+++ aws/_modules/rds/rds-instance/main.tf
@@ -170,6 +170,7 @@ POLICY
 # KMS
 # ---------------------------------------------------------------------
 resource "aws_kms_key" "TerraFailDB_key" {
+  # Drata: Define [aws_kms_key.policy] to restrict access to your resource. Follow the principal of minimum necessary access, ensuring permissions are scoped to trusted entities. Exclude this finding if access to Keys is managed using IAM policies instead of a Key policy
   description             = "TerraFailDB key description"
   deletion_window_in_days = 10
   enable_key_rotation     = true
