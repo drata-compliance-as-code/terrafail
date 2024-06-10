c837e6b989ab22c635c6429f66a661a5c68eefdd
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:06 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/container/main.tf
+++ gcp/_modules/container/main.tf
@@ -2,6 +2,7 @@
 # Container
 # ---------------------------------------------------------------------
 resource "google_container_cluster" "TerraFailContainer_cluster" {
+  # Drata: Set [configId] to ensure that organization-wide label conventions are followed.
   name                    = "TerraFailContainer_cluster"
   initial_node_count      = 3
   enable_kubernetes_alpha = true
