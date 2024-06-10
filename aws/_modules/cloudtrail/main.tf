9ea6622af452232a2c4fd6ae6bf51640774efb93
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/cloudtrail/main.tf
+++ aws/_modules/cloudtrail/main.tf
@@ -5,7 +5,7 @@
 resource "aws_cloudtrail" "sac_cloudtrail" {
   name                          = "sac_cloudtrail"
   s3_bucket_name                = aws_s3_bucket.sac_testing.id
-  enable_log_file_validation = false
+  enable_log_file_validation = true
   is_multi_region_trail = false
 }
 
