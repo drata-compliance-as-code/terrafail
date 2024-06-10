70488cb0e9e7411379fe35478511e6b9ea1a0ed6
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/dns/main.tf
+++ azure/_modules/dns/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_resource_group" "TerraFailDNS_rg" {
 # DNS
 # ---------------------------------------------------------------------
 resource "azurerm_dns_zone" "TerraFailDNS" {
+  # Drata: Set [azurerm_dns_zone.tags] to ensure that organization-wide tagging conventions are followed.
   name                = "thisisthedarkside.com"
   resource_group_name = azurerm_resource_group.TerraFailDNS_rg.name
 }
