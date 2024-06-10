e7ef5c027016f1200337aedb94d76681ae710f7f
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/elasticsearch/main.tf
+++ aws/_modules/elasticsearch/main.tf
@@ -30,7 +30,7 @@ resource "aws_elasticsearch_domain" "TerraFailElasticache_domain" {
   }
 
   domain_endpoint_options {
-    enforce_https       = false
+    enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
   }
 
