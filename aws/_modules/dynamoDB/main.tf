38b90d2697ca67ac00a9bbb17bbde24d7c891286
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/dynamoDB/main.tf
+++ aws/_modules/dynamoDB/main.tf
@@ -60,6 +60,7 @@ resource "aws_dynamodb_table" "TerraFailDynamoDB" {
 # KMS
 # ---------------------------------------------------------------------
 resource "aws_kms_key" "TerraFailDynamoDB_key" {
+  # Drata: Define [aws_kms_key.policy] to restrict access to your resource. Follow the principal of minimum necessary access, ensuring permissions are scoped to trusted entities. Exclude this finding if access to Keys is managed using IAM policies instead of a Key policy
   description             = "TerraFailDynamoDB encryption key"
   deletion_window_in_days = 10
   enable_key_rotation     = false
