771c1637aeeb7724d0a745da22b2aae4e5a2db40
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/aks/main.tf
+++ azure/_modules/aks/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_resource_group" "TerraFailAKS_rg" {
 # AKS
 # ---------------------------------------------------------------------
 resource "azurerm_kubernetes_cluster" "TerraFailAKS_cluster" {
+  # Drata: Set [azurerm_kubernetes_cluster.automatic_channel_upgrade] to any of ['stable', 'rapid', 'patch'] to automatically upgrade AKS cluster to the latest Kubernetes version
   # Drata: Set [azurerm_kubernetes_cluster.role_based_access_control_enabled] to true to configure resource authentication using role based access control (RBAC). RBAC allows for more granularity when defining permissions for users and workloads that can access a resource. Set [microsoft_container_service.managed_clusters.disable_local_accounts] to true to restrict access to your cluster through local admin accounts. If configuring this setting on an existing cluster, be sure to rotate cluster certificates to revoke any certificates that previously authenticated users may have had access to
   # Drata: Set [azurerm_kubernetes_cluster.tags] to ensure that organization-wide tagging conventions are followed.
   # Drata: Set [azurerm_kubernetes_cluster.network_profile.network_policy] to any of ['azure', 'calico', 'cilium'] to define access policy specifications for communication between Pods
