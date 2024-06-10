8e210a370bd6125eee05b683ae8a7148e81e470d
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- azure/_modules/functions/main.tf
+++ azure/_modules/functions/main.tf
@@ -69,6 +69,7 @@ resource "azurerm_service_plan" "TerraFailFunction_service_plan" {
 }
 
 resource "azurerm_storage_account" "TerraFailFunction_storage_linux" {
+  # Drata: Set [azurerm_storage_account.public_network_access_enabled] to false to prevent unintended public access. Ensure that only trusted users and IP addresses are explicitly allowed access, if a publicly accessible service is required for your business use case this finding can be excluded
   # Drata: Set [azurerm_storage_account.enable_https_traffic_only] to true to ensure secure protocols are being used to encrypt resource traffic
   name                     = "TerraFailFunction_storage_linux"
   resource_group_name      = azurerm_resource_group.TerraFailFunction_rg.name
