bbb29001af90b50fba75f748dc6eeafce251e51a
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:06 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/sns/main.tf
+++ aws/_modules/sns/main.tf
@@ -5,6 +5,7 @@ resource "aws_sns_topic" "TerraFailSNS" {
   name         = "TerraFailSNS"
   display_name = "TerraFailSNS"
   policy       = <<EOF
+  # Drata: Define [aws_sns_topic.policy] to restrict access to your resource. Follow the principal of minimum necessary access, ensuring permissions are scoped to trusted entities
 {
     "Version": "2012-10-17",
     "Statement": [
