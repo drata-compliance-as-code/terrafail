e1b9db84db0ef678c99c19598394e10f7f7018fe
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/lambda/main.tf
+++ aws/_modules/lambda/main.tf
@@ -109,6 +109,7 @@ resource "aws_subnet" "TerraFailLambda_subnet" {
 }
 
 resource "aws_vpc" "TerraFailLambda_vpc" {
+  # Drata: Set [aws_vpc.tags] to ensure that organization-wide tagging conventions are followed.
   cidr_block = "10.0.0.0/16"
 }
 
