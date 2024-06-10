a395a42e5ecb7c44185fbecbef9678dacbfdeb15
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/cloudfront/main.tf
+++ aws/_modules/cloudfront/main.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "TerraFailCloudfront_distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "http-only"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1"]
     }
   }
