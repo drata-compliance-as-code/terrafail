8f7754ca95585b9d3d696dc5220f6a9ded7073e8
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:06 +0000

    Drata Compliance Recommendations (Grouped)

--- gcp/_modules/storage/main.tf
+++ gcp/_modules/storage/main.tf
@@ -2,6 +2,7 @@
 # Storage
 # ---------------------------------------------------------------------
 resource "google_storage_bucket" "TerraFailStorage" {
+  # Drata: Set [configId] to ensure that organization-wide label conventions are followed.
   name                        = "TerraFailStorage"
   location                    = "US-EAST1"
   uniform_bucket_level_access = true
