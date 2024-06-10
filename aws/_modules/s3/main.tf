b5cc93249eac7f7dc5f7b722d4aee5b7875f2064
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:04 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/s3/main.tf
+++ aws/_modules/s3/main.tf
@@ -58,7 +58,7 @@ EOF
 
 resource "aws_s3_bucket_public_access_block" "TerraFailS3_bucket_access" {
   bucket                  = aws_s3_bucket.TerraFailS3_bucket.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
