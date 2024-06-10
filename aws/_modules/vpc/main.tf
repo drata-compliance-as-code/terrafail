2e697efbc8ee23fde4558ccbd3fd87413209f55a
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-06-10 22:27:05 +0000

    Drata Compliance Recommendations (Grouped)

--- aws/_modules/vpc/main.tf
+++ aws/_modules/vpc/main.tf
@@ -14,6 +14,7 @@ resource "aws_vpc" "TerraFailVPC_peer" {
 }
 
 resource "aws_vpc_peering_connection" "TerraFailVPC_peering_connection" {
+  # Drata: Set [aws_vpc_peering_connection.tags] to ensure that organization-wide tagging conventions are followed.
   vpc_id      = aws_vpc.TerraFailVPC.id
   peer_vpc_id = aws_vpc.TerraFailVPC_peer.id
 }
